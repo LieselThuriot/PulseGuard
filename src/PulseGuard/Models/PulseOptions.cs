10a0a86fc1577fbaaa058d65ebd9f047e31d3d6c
Liesel Thuriot <git@thuriot.be>
2025-11-19 15:37:20 +0100

    Fixed the eager blob archiving

--- src/PulseGuard/Models/PulseOptions.cs
+++ src/PulseGuard/Models/PulseOptions.cs
@@ -4,7 +4,7 @@ public class PulseOptions
 {
     public int Interval { get; set => field = Math.Max(1, value); } = 1;
     public int CleaningInterval { get; set => field = Math.Max(1, value); } = 13;
-    public int SimultaneousPulses { get; set => field = Math.Max(1, value); } = 5;
+    public int SimultaneousPulses { get; set => field = Math.Max(1, value); } = 20;
     public int? AlertThreshold { get; set; }
     public string Store { get; set; } = "";
 }
